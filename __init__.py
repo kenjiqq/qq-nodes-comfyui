04da027bd78f018d318cfac6a55fc4fd1f2b689f
kenjiqq <599468+kenjiqq@users.noreply.github.com>
2024-09-01 02:53:12 +0200

    change the way the extension javascript is loaded

--- __init__.py
+++ __init__.py
@@ -1,9 +1,15 @@
-import os, shutil
-import folder_paths
 from .nodes import NODE_CLASS_MAPPINGS
 
-module_js_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "js")
+import os
+import folder_paths
+
+# In the past we installed the extension by copying the extensions.js file to the web/extensions/qq-nodes folder, so we need to delete it if it exists when users upgrade to the new version.
 application_root_directory = os.path.dirname(folder_paths.__file__)
 application_web_extensions_directory = os.path.join(application_root_directory, "web", "extensions", "qq-nodes")
+file_to_delete = os.path.join(application_web_extensions_directory, "extension.js")
+if os.path.exists(file_to_delete):
+    os.remove(file_to_delete)
 
-shutil.copytree(module_js_directory, application_web_extensions_directory, dirs_exist_ok=True)
+WEB_DIRECTORY = "./js"
+
+__all__ = ["NODE_CLASS_MAPPINGS", "WEB_DIRECTORY"]
